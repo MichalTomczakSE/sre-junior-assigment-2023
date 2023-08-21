fa3394e536264214358cf85187eaa401c5de0c61
Michal Tomczak <MichalTomczakSE@gmail.com>
2023-08-21 23:20:34 +0000

    Hotfix - change --user-agent method

--- assigment-1/script.sh
+++ assigment-1/script.sh
@@ -12,15 +12,11 @@ case $OPTION in
         echo "You need to provide argument for --user-agent option. --user-agent <parameter>"
         rm -rf $(pwd)/logs && exit 1
     fi
-    printf "%-30s %-30s %s\n"  "ADDRESS" "COUNT" "USER-AGENT" > Parsed_User_Agent_Log.txt
-    paste -d ' ' <(grep -oP 'client_ip: "\K[^"]+' $LOGFILE) <(grep -oP 'user_agent: "\K[^"]+' $LOGFILE) | \
-    grep $VALUE | uniq -c | \
-    awk '{  concat = $3; 
-            for (i=4; i<=NF; i++) 
-            concat = concat " " $i; 
-            printf "%-30s %-30s %s\n", $2, $1, concat 
-        }' >> Parsed_User_Agent_Log.txt
-;;
+    	printf "%-30s %-30s %s\n" "ADDRESS" "USER_AGENT" "COUNT" > Parsed_User_Agent_Log.txt
+	grep -i "user_agent: .*$VALUE." $LOGFILE | awk -F 'client_ip' '{ gsub(/"/, "", $2); print $2 }' | \
+	sort | uniq -c | awk '{printf "%-30s %-30s %d\n", $3,$5,$1 }' >> Parsed_User_Agent_Log.txt
+	
+	;;
  "--method")
     printf "%-30s %-30s %s\n" "ADDRESS" "METHOD" "COUNT" > Parsed_Method_Log.txt
     paste -d ' ' <(grep -oP 'client_ip: "\K[^"]+' $LOGFILE) <(grep -oP 'method: "\K[^"]+' $LOGFILE) | \
